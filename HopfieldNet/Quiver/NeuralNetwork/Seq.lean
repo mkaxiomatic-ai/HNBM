98a74db77d234ecdeb7f97c727d00e603eb89142
Anon <40603357+mkaratarakis@users.noreply.github.com>
2026-03-09 20:11:45 +0100

    Update Seq.lean

--- HopfieldNet/Quiver/NeuralNetwork/Seq.lean
+++ HopfieldNet/Quiver/NeuralNetwork/Seq.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Michail Karatarakis. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Michail Karatarakis
+-/
 import Mathlib.Analysis.SpecialFunctions.Exp
 import HopfieldNet.Quiver.NeuralNetwork.Main
 

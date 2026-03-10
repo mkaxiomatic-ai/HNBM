70ebc4b945e7f50613e5919351211c2a76752f02
Anon <40603357+mkaratarakis@users.noreply.github.com>
2026-03-10 16:23:58 +0100

    Add copyright and license information to Core.lean

--- HopfieldNet/Quiver/HN/Core.lean
+++ HopfieldNet/Quiver/HN/Core.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Michail Karatarakis. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Michail Karatarakis
+-/
 import Mathlib.LinearAlgebra.Matrix.Symmetric
 import Mathlib.Data.Matrix.Reflection
 import Mathlib.Data.Vector.Defs
